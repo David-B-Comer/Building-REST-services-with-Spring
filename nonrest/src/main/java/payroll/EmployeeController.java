534bd430b5a1442d4d1cc5e967b36e2b402ba1d4
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-06-03 20:03:23 -0400

    added non rest spring hateoas to pom

--- nonrest/src/main/java/payroll/EmployeeController.java
+++ nonrest/src/main/java/payroll/EmployeeController.java
@@ -1,7 +1,6 @@
 package payroll;
 
 import java.util.List;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 class EmployeeController {
 
     private final EmployeeRepository repository;
